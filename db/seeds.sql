a758bfecea8a8b9f95e9c7e69c64965c7e9c215c
unknown <ming29398@gmail.com>
2018-11-12 20:32:54 -0800

    update database schema, data, and images

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (BURGER_NAME) values ('Hamburger');
+INSERT INTO burgers (BURGER_NAME) values ('Cheeseburger');
+INSERT INTO burgers (BURGER_NAME) values ('Bacon Cheeseburger');
+INSERT INTO burgers (BURGER_NAME) values ('Double Bacon Cheeseburger');
