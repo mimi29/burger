0825615ea9680e5d425d7b1f3e09b05296426e8b
unknown <ming29398@gmail.com>
2018-11-18 20:01:26 -0800

    added jaws db to server

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var express = require("express");
-
+var mysql = require('mysql');
+if (process.env.JAWSDB_URL) {
+  var connection=mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+  var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Summer2018!!',
+    database:'burgers_db'
+  })
+}
 var PORT = process.env.PORT || 8080;
 
 var app = express();
